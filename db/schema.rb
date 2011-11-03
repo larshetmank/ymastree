568939e5b7fecdc6278f22ce1eed8ac7eae734f9
Lars Hetmank <entwicklungssoftware@web.de>
2011-11-03 22:28:01 +0100

    User model with password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111103185517) do
+ActiveRecord::Schema.define(:version => 20111103210628) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(:version => 20111103185517) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
 end
